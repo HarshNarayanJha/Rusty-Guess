282ca1f05ad7541f31117f43db8ff9d5785e0f1b
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-07-16 12:19:49 +0530

    multiple rounds

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
-    println!("Guess the number");
+    println!("Guess the number\n");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let mut rounds = String::new();
+    println!("How many random numbers do you want to guess?");
+    io::stdin()
+        .read_line(&mut rounds)
+        .expect("Error reading input!");
 
+    let rounds: u8 = rounds.trim().parse().expect("Error parsing rounds");
+
+    let mut secret_numbers = Vec::new();
+
+    for _ in 0..rounds {
+        let secret_number = rand::thread_rng().gen_range(1..=100);
+        secret_numbers.push(secret_number)
+    }
     // println!("The secret number is: {secret_number}");
 
-    loop {
-        println!("Please input your guess.");
+    let mut guesses_made = 0;
+
+    println!("Please input your guess.");
+    while guesses_made < rounds {
 
         let mut guess = String::new();
 
@@ -25,13 +39,21 @@ fn main() {
 
         println!("You guessed: {guess}");
 
-        match guess.cmp(&secret_number) {
+        match guess.cmp(&secret_numbers[guesses_made as usize]) {
             Ordering::Less => println!("Too small"),
             Ordering::Greater => println!("Too big"),
             Ordering::Equal => {
                 println!("You win");
-                break;
-            },
+                guesses_made += 1;
+                if guesses_made < rounds {
+                    println!("Now Guess the next number, round #{}", guesses_made + 1);
+                }
+            }
         }
+    }
+
+    println!("Thanks for playing! The correct answers were: ");
+    for item in secret_numbers {
+        println!("{item}");
     }
 }
